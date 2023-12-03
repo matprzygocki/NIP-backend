c7c9cdabdb638ae945707e82b5e16f55db5565e9
Michał Ladra <michal.ladra.95@gmail.com>
2023-12-03 22:07:51 +0100

    Autoryzacja poprzez API_KEY

--- ai-rest-app/main.py
+++ ai-rest-app/main.py
@@ -1,10 +1,12 @@
-from fastapi import FastAPI, UploadFile, File
+from fastapi import FastAPI, UploadFile, File, HTTPException, Security
 from fastapi.encoders import jsonable_encoder
 from fastapi.responses import JSONResponse
+from starlette import status
 from starlette.middleware.cors import CORSMiddleware
 
 import prediction_service as service
 from predefined_learning_files import PredefinedLearningFile
+from fastapi.security import APIKeyHeader
 
 app = FastAPI()
 
@@ -21,6 +23,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+api_key_header = APIKeyHeader(name="X-API-Key")
+api_keys = [
+    "4e9d8822-2eaf-46c4-b48c-4e9bdb3317c4"
+]
+
+
+def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
+    if api_key_header in api_keys:
+        return api_key_header
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid or missing API Key"
+    )
+
 
 @app.get("/health")
 def health():
@@ -28,14 +44,14 @@ def health():
 
 
 @app.post("/predict/{predefined_file}", response_class=JSONResponse)
-def post_predict_existing_file(predefined_file: PredefinedLearningFile, split_percentage: float = 0.67):
+def post_predict_existing_file(predefined_file: PredefinedLearningFile, split_percentage: float = 0.67, api_key: str = Security(get_api_key)):
     result = service.predict(predefined_file.path(), split_percentage)
     result_map_encoded = jsonable_encoder(result)
     return JSONResponse(content=result_map_encoded)
 
 
 @app.post("/predict", response_class=JSONResponse)
-async def post_predict(file: UploadFile = File(...), split_percentage: float = 0.67):
+async def post_predict(file: UploadFile = File(...), split_percentage: float = 0.67, api_key: str = Security(get_api_key)):
     result = service.predict(file.file, split_percentage)
     result_map_encoded = jsonable_encoder(result)
     return JSONResponse(content=result_map_encoded)
